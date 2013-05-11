adc861cb3f1bb61d99bcb9bf52995bd4b92c9cc5
Ross Hadden <rosshadden@gmail.com>
2013-05-10 22:38:15 -0400

    Hi, Mom!

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+	routes = require('./routes'),
+	user = require('./routes/user'),
+	http = require('http'),
+	path = require('path');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('shhhh, it is a secret!'));
+app.use(express.session());
+app.use(app.router);
+	app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
