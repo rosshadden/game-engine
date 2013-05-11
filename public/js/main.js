f34572ff7f5bb19aed3142997a766a0b31d8a498
Ross Hadden <rosshadden@gmail.com>
2013-05-10 22:50:37 -0400

    Added jquery.

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,10 @@
-console.log('hello');
+require.config({
+	packages: ['entities', 'utilities'],
+	paths: {
+		'text': 'lib/require.text'
+	},
+	deps: ['utilities/log', 'lib/three', 'lib/jquery'],
+	callback: function(log){
+		window.log = log;
+	}
+});
